19509f688c0e4ec7a7538ed006516026caa5eaf6
diewland <diewland@gmail.com>
2024-08-31 11:22:56 +0700

    update token name

--- contracts/BLZ.sol
+++ contracts/BLZ.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract BLZ is ERC20 {
-    constructor() ERC20("BLZ", "BLZ") {
+    constructor() ERC20("BLOBz", "BLZ") {
         _mint(msg.sender, 1_000_000_000 * 10 ** decimals());
     }
 }
