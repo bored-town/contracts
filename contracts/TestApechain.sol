d873732d2a3f91020b0fc9611e409e118fae6a8a
diewland <diewland@gmail.com>
2024-09-16 10:56:13 +0700

    add test apechain contract

---
+++ contracts/TestApechain.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract TestApechain is ERC721A, Ownable {
+
+    // config
+    constructor(address initialOwner) ERC721A("Test APECHAIN", "TESTAPECHAIN") Ownable(initialOwner) {}
+    uint256 public MAX_SUPPLY = 10_000;
+    uint256 public MAX_MINT_PER_WALLET = 10;
+    uint256 public START_ID = 1;
+
+    bool public mintEnabled = true;
+    string public baseURI = "https://cryptologos.cc/logos/apecoin-ape-ape-logo.png";
+
+    // start token id
+    function _startTokenId() internal view virtual override returns (uint256) {
+        return START_ID;
+    }
+
+    // metadata
+    function setBaseURI(string calldata _newBaseURI) external onlyOwner {
+        baseURI = _newBaseURI;
+    }
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        string memory jsonPreImage = string.concat(
+            string.concat(
+                string.concat('{"name": "Test APECHAIN #', Strings.toString(tokenId)),
+                '","description":"Test deploy NFT on APECHAIN","image":"'
+            ),
+            baseURI
+        );
+        string memory jsonPostImage = '"}';
+        return
+            string.concat(
+                "data:application/json;utf8,",
+                string.concat(jsonPreImage, jsonPostImage)
+            );
+    }
+
+    // toggle sale, round
+    function toggleSale() external onlyOwner {
+        mintEnabled = !mintEnabled;
+    }
+
+    // mint
+    function mint(uint quantity, bytes32[] calldata _merkleProof) external {
+        require(mintEnabled, "Sale is not enabled");
+        require(_numberMinted(msg.sender) + quantity <= MAX_MINT_PER_WALLET, "Over wallet limit");
+        
+        _checkSupplyAndMint(msg.sender, quantity);
+    }
+    function adminMint(uint quantity) external onlyOwner {
+        _checkSupplyAndMint(msg.sender, quantity);
+    }
+    function _checkSupplyAndMint(address to, uint256 quantity) private {
+        require(_totalMinted() + quantity <= MAX_SUPPLY, "Over supply");
+
+        _mint(to, quantity);
+    }
+
+    // aliases
+    function numberMinted(address owner) external view returns (uint256) {
+        return _numberMinted(owner);
+    }
+    function remainingSupply() external view returns (uint256) {
+        return MAX_SUPPLY - _totalMinted();
+    }
+
+}
